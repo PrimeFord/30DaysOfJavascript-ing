61a20d79c018015d09bd14c7a2f85edf15eb0ae9
PrimeFord <bashfordbash@gmail.com>
2022-11-18 15:27:19 +0100

    feat:Day3 Act3 Completed

---
+++ day3/Act3/script.js
@@ -0,0 +1,13 @@
+//1.Create a human readable time format using the Date time object. The hour and the minute should be all the time two digits(7 hours should be 07 and 5 minutes should be 05 )
+//i. YYY-MM-DD HH:mm eg. 20120-01-02 07:05
+const present = new Date();
+const year = present.getFullYear();
+const month = present.getMonth();
+const date = present.getDate();
+const hour = present.getHours();
+const minute = present.getMinutes();
+
+hour < 10 ? console.log(`0${hour}`) : console.log(`${hour}`);
+minute < 10 ? console.log(`0${minute}`) : console.log(`${minute}`);
+
+console.log(`${year}-${month}-${date} ${hour}:${minute}`);
