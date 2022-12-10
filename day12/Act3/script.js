65ea3560ef293af90a7ea5e68e41484a099b98bc
PrimeFord <bashfordbash@gmail.com>
2022-12-10 17:29:57 +0100

    feat:Day12 Act3 Completed

---
+++ day12/Act3/script.js
@@ -0,0 +1,9 @@
+//Write a function which cleans text. Clean the following text. After cleaning, count three most frequent words in the string.
+
+const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+
+function cleanText(n) {
+  clean = n.replace(/%|$|@|&|#/g, "");
+  return clean;
+}
+console.log(cleanText(sentence));
