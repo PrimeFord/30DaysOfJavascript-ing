1aa5560aee8838c5de88244516452bc5c70169d5
PrimeFord <bashfordbash@gmail.com>
2022-12-10 17:06:13 +0100

    feat:Day12 Act2 Completed

---
+++ day12/Act2/script.js
@@ -0,0 +1,3 @@
+//Write a function called tenMostFrequentWords which get the ten most frequent word from a string?
+paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`;
+console.log(tenMostFrequentWords(paragraph));
