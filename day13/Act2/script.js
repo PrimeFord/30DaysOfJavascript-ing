bdedc5809d74a2f69f6bfa402e00d175617eedb0
PrimeFord <bashfordbash@gmail.com>
2022-12-11 08:42:46 +0100

    feat:Day13 Act2 Completed

---
+++ day13/Act2/script.js
@@ -0,0 +1,8 @@
+//1. 10 > 2 * 10 use console.assert()
+console.assert(10 > 2 * 10, "10 is not greater than 2*10(=20)");
+
+//2. Write a warning message using console.warn()
+console.warn("Don't f**k with me");
+
+//3. Write an error message using console.error()
+console.error("You speaking in the rubbish");
