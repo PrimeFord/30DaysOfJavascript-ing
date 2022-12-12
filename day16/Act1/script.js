fff2d3737f1d7555c8ac13ba32a33cc5c7991dcb
PrimeFord <bashfordbash@gmail.com>
2022-12-12 13:49:26 +0100

    feat:Day16 Act1 Completed

--- day16/Act1/script.js
+++ day16/Act1/script.js
@@ -1,15 +1,19 @@
-const skills = ["HTML", "CSS", "JS", "React", "Node", "Python"];
-
 //1. Change skills array to JSON using JSON.stringify()
-
-let age = 250;
+const skills = ["HTML", "CSS", "JS", "React", "Node", "Python"];
+let skill = JSON.stringify(skills, undefined, 4);
+console.log(skill);
 
 //2. Stringify the age variable
-
-let isMarried = true;
+let age = 250;
+let ages = JSON.stringify(age);
+console.log(ages);
 
 //3. Stringify the isMarried variable
+let isMarried = true;
+let isMarriedd = JSON.stringify(isMarried);
+console.log(isMarriedd);
 
+//4. Stringify the student object
 const student = {
   firstName: "Asabeneh",
   lastName: "Yetayehe",
@@ -17,100 +21,6 @@ const student = {
   isMarried: true,
   skills: ["HTML", "CSS", "JS", "React", "Node", "Python"],
 };
-const txt = `{
-      "Alex": {
-          "email": "alex@alex.com",
-          "skills": [
-              "HTML",
-              "CSS",
-              "JavaScript"
-          ],
-          "age": 20,
-          "isLoggedIn": false,
-          "points": 30
-      },
-      "Asab": {
-          "email": "asab@asab.com",
-          "skills": [
-              "HTML",
-              "CSS",
-              "JavaScript",
-              "Redux",
-              "MongoDB",
-              "Express",
-              "React",
-              "Node"
-          ],
-          "age": 25,
-          "isLoggedIn": false,
-          "points": 50
-      },
-      "Brook": {
-          "email": "daniel@daniel.com",
-          "skills": [
-              "HTML",
-              "CSS",
-              "JavaScript",
-              "React",
-              "Redux"
-          ],
-          "age": 30,
-          "isLoggedIn": true,
-          "points": 50
-      },
-      "Daniel": {
-          "email": "daniel@alex.com",
-          "skills": [
-              "HTML",
-              "CSS",
-              "JavaScript",
-              "Python"
-          ],
-          "age": 20,
-          "isLoggedIn": false,
-          "points": 40
-      },
-      "John": {
-          "email": "john@john.com",
-          "skills": [
-              "HTML",
-              "CSS",
-              "JavaScript",
-              "React",
-              "Redux",
-              "Node.js"
-          ],
-          "age": 20,
-          "isLoggedIn": true,
-          "points": 50
-      },
-      "Thomas": {
-          "email": "thomas@thomas.com",
-          "skills": [
-              "HTML",
-              "CSS",
-              "JavaScript",
-              "React"
-          ],
-          "age": 20,
-          "isLoggedIn": false,
-          "points": 40
-      },
-      "Paul": {
-          "email": "paul@paul.com",
-          "skills": [
-              "HTML",
-              "CSS",
-              "JavaScript",
-              "MongoDB",
-              "Express",
-              "React",
-              "Node"
-          ],
-          "age": 20,
-          "isLoggedIn": false,
-          "points": 40
-      }
-  }
-  `;
-//4. Stringify the student object
+
+let stuents = JSON.stringify(student);
+console.log(stuents);
