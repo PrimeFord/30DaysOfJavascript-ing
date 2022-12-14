999e7848a693b05ffb198e28978f0e3493bdb165
PrimeFord <bashfordbash@gmail.com>
2022-12-14 15:42:03 +0100

    feat:Day15 Act2 Completed

--- day15/Act2/script.js
+++ day15/Act2/script.js
@@ -1,16 +1,42 @@
 //1. Override the method you create in Animal class
-class Dog extends Animal {
-  constructor(name, age, color, legs, gender) {
-    super(name, age, color, legs);
-    this.gender = gender;
+class Animal {
+  constructor(name, age, color, legs) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.legs = legs;
   }
-  dogInfo() {
-    console.log("This dog is a child from the animal class");
+  getName() {
+    const name = this.name;
+    return name;
+  }
+  getAge() {
+    const Age = this.age;
+    return Age;
+  }
+  getColor() {
+    const color = this.color;
+    return color;
+  }
+  getLegs() {
+    const legs = this.legs;
+    return legs;
   }
   getInfo() {
     const info = `the name of this dog is ${this.name}, it is ${this.age} year(s) old. it is ${this.color} in color and it has ${this.legs} legs`;
     return info;
   }
 }
-const dog = new Animal("Ghost", "3", "Grey", 4);
-console.log(dog.dogInfo);
+class Dog extends Animal {
+  constructor(name, age, color, legs, gender) {
+    super(name, age, color, legs);
+    this.gender = gender;
+  }
+
+  getInfo() {
+    const info = `This dog is a child from the animal class. The name of this dog is ${this.name}, it is ${this.age} year(s) old. it is ${this.color} in color and it has ${this.legs} legs`;
+    return info;
+  }
+}
+const dog = new Dog("Ghost", "3", "Grey", 4);
+console.log(dog.getInfo());
