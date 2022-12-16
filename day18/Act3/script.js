600db18bf35b12a124f9c1b3b223f9f1969cd5dc
PrimeFord <bashfordbash@gmail.com>
2022-12-16 14:54:44 +0100

    feat:Day18 Act3 Completed

---
+++ day18/Act3/script.js
@@ -0,0 +1,3 @@
+//1. Read the cats api and find the average weight of cat in metric unit.
+//2. Read the countries api and find out the 10 largest countries
+//3. Read the countries api and count total number of languages in the world used as officials.
