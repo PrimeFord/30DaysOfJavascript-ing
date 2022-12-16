984f563bd80cd36cedaee9cae84a9b638dc473a1
PrimeFord <bashfordbash@gmail.com>
2022-12-16 16:29:52 +0100

    feat:Day18 Act 1 Completed

--- day18/Act1/script.js
+++ day18/Act1/script.js
@@ -1,15 +1,17 @@
-const catsAPI = "https://api.thecatapi.com/v1/breeds";
-
 //1.Read the countries API using fetch and print the name of country, capital, languages, population and area.
 const countriesAPI = "https://restcountries.com/v2/all";
+// const fetch = require("fetch");
+// import fetch from "fetch";
 fetch(countriesAPI)
   .then((response) => response.JSON)
-  .then((data) => {
-    console.log(data);
-  });
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+//OR
+
 const fetchData = async () => {
   try {
-    const response = await fetch(url);
+    const response = await fetch(countriesAPI);
     const countries = await response.json();
     console.log(countries);
   } catch (err) {
