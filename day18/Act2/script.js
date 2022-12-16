fb0bbf6adb413f76f425005b541a8a60761a9aa3
PrimeFord <bashfordbash@gmail.com>
2022-12-16 16:38:14 +0100

    feat:Day18 Act2 Completed

--- day18/Act2/script.js
+++ day18/Act2/script.js
@@ -1,1 +1,18 @@
+const catsAPI = "https://api.thecatapi.com/v1/breeds";
+
 //1. Print out all the cat names in to catNames variable.
+const fetchData = async () => {
+  try {
+    const response = await fetch(catsAPI);
+    const cats = await response.json();
+    // console.log(cats);
+    let cat = JSON.parse(cats);
+    for (const c of cat) {
+      let catName = c.name;
+      console.log(catName);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+fetchData();
