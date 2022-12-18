cc02dd4cbf4741d78968505f24770f8dde7bf7ef
PrimeFord <bashfordbash@gmail.com>
2022-12-18 23:59:38 +0100

    feat:Day19 Act3 Completed

--- day19/Act3/script.js
+++ day19/Act3/script.js
@@ -9,35 +9,31 @@ const personAccount = () => {
   let expenses = [1100, 32435, 525, 564, 54786, 234];
   totalIncomes = () => {
     let sum = 0;
-    for (let i = 0; i < this.incomes.length; i++) {
-      sum += this.incomes[i];
+    for (let i = 0; i < incomes.length; i++) {
+      sum += incomes[i];
     }
     return sum;
   };
   totalExpenses = () => {
     let sum = 0;
-    for (let i = 0; i < this.expenses.length; i++) {
-      sum += this.expenses[i];
+    for (let i = 0; i < expenses.length; i++) {
+      sum += expenses[i];
     }
     return sum;
   };
   accountInfo = () => {
-    return `${this.firstName} ${this.lastName} incomes are ${
-      this.incomes
-    }, expenses ${
-      this.expenses
-    }. he has a total income of ${this.totalIncomes()} and total expenses of ${this.totalExpenses()} which leaves him with an Account balance of ${this.accountBalance()}`;
+    return `${firstName} ${lastName} incomes are ${incomes}, expenses ${expenses}. he has a total income of ${totalIncomes()} and total expenses of ${totalExpenses()} which leaves him with an Account balance of ${accountBalance()}`;
   };
   addIncome = (x) => {
-    this.incomes.push(x);
+    incomes.push(x);
     return this.incomes;
   };
   addExpenses = (y) => {
-    this.expenses.push(y);
+    expenses.push(y);
     return this.expenses;
   };
   accountBalance = () => {
-    ac = this.totalIncomes() - this.totalExpenses();
+    ac = totalIncomes() - totalExpenses();
     return ac;
   };
   return {
@@ -48,19 +44,19 @@ const personAccount = () => {
     totalIncomes: totalIncomes(),
     totalExpenses: totalExpenses(),
     accountInfo: accountInfo(),
-    addIncome: addIncome(),
-    addExpenses: addExpenses(),
+    addIncome: addIncome(1000000),
+    addExpenses: addExpenses(1000),
     accountBalance: accountBalance(),
   };
 };
 const personalAccount = personAccount();
-console.log(personalAccount.firstName());
-console.log(personalAccount.lastName());
-console.log(personalAccount.incomes());
-console.log(personalAccount.expenses());
-console.log(personalAccount.totalIncomes());
-console.log(personalAccount.totalExpenses());
-console.log(personalAccount.accountInfo());
-console.log(personalAccount.addIncome(1000000));
-console.log(personalAccount.addExpenses(1000));
-console.log(personalAccount.accountBalance());
+console.log(personalAccount.firstName);
+console.log(personalAccount.lastName);
+console.log(personalAccount.incomes);
+console.log(personalAccount.expenses);
+console.log(personalAccount.totalIncomes);
+console.log(personalAccount.totalExpenses);
+console.log(personalAccount.accountInfo);
+console.log(personalAccount.addIncome);
+console.log(personalAccount.addExpenses);
+console.log(personalAccount.accountBalance);
