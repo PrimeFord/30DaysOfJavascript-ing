7bdfbc8bf6860c44e6e4ecdb493f0a59276864fc
PrimeFord <bashfordbash@gmail.com>
2022-12-19 12:17:23 +0100

    feat:Day21 Act2 Completed

---
+++ day21/Act2/script.js
@@ -0,0 +1,3 @@
+//1. Change stye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
+//2. Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
+//3. Set text content, id and class to each paragraph
