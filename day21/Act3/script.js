e1959ac15a3eb13ca4e8356363d94c79f9130b6c
PrimeFord <bashfordbash@gmail.com>
2022-12-19 15:29:40 +0100

    feat:Day21 Act3 Completed

--- day21/Act3/script.js
+++ day21/Act3/script.js
@@ -3,6 +3,7 @@
 bodyEl = document.querySelector("body");
 bodyEl.style.width = "100vw";
 bodyEl.style.fontFamily = "Segoe UI";
+bodyEl.style.cursor = "pointer";
 
 wrpEl = document.querySelector(".wrapper");
 wrpEl.style.width = "50%";
@@ -10,14 +11,16 @@ wrpEl.style.margin = "auto";
 
 h1El = document.querySelector("h1");
 h1El.style.textAlign = "center";
+h1El.style.fontSize = "1.5rem";
 h2El = document.querySelector("h2");
 h2El.style.textAlign = "center";
+h2El.style.fontSize = "1.2rem";
 h2El.style.textDecoration = "underline";
 h2El.style.fontWeight = "100";
 
 //i.The year color is changing every 1 second
 const spanEl = document.querySelector("span");
-spanEl.style.fontSize = "4rem";
+spanEl.style.fontSize = "3rem";
 
 //ii.The date and time background color is changing every on seconds
 const monthNames = [
@@ -47,9 +50,31 @@ let se = sec < 10 ? `0${sec}` : sec;
 let date = `${monthNames[month]} ${day}, ${year} ${hr}:${mi}:${se}`;
 console.log(date);
 
-wrpEl.innerHTML = `<p>${date}</p>`;
+const pEl = document.querySelector("p");
+pEl.innerHTML = date;
+pEl.style.fontSize = "0.8rem";
+pEl.style.textAlign = "center";
+pEl.style.margin = "auto";
+pEl.style.padding = "0.3rem 1rem";
+pEl.style.width = "max-content";
+pEl.style.background = "pink";
 
-// let year = now.getFullYear;
+const randBg = () => {
+  spanEl.style.color = "#" + Math.random().toString(16).substr(2, 6);
+  pEl.style.background = "#" + Math.random().toString(16).substr(2, 6);
+};
+setInterval(randBg, 1000);
+
 //iii.Completed challenge has background green
 //iv.Ongoing challenge has background yellow
 //v.Coming challenges have background red
+const liEl = document.querySelectorAll("li");
+for (let i = 0; i < liEl.length; i++) {
+  liEl[i].style.background = "#eb695b";
+  liEl[i].style.listStyle = "none";
+  liEl[i].style.marginBottom = "0.2rem";
+  liEl[i].style.padding = "0.8rem";
+  liEl[i].style.fontSize = "0.8rem";
+  liEl[0].style.background = "#5bb67a";
+  liEl[1].style.background = "#f7dc5c";
+}
