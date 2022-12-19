03fa6018bdf14bee6b976e2acb296cd4afb5b251
PrimeFord <bashfordbash@gmail.com>
2022-12-19 11:47:14 +0100

    feat:Day21 Act1 Completed

---
+++ day21/Act1/script.js
@@ -0,0 +1,6 @@
+//1. Create an index.html file and put four p elements as above: Get the first paragraph by using document.querySelector(tagname) and tag name
+//2. Get each of the the paragraph using document.querySelector('#id') and by their id
+//3. Get all the p as nodeList using document.querySelectorAll(tagname) and by their tag name
+//4. Loop through the nodeList and get the text content of each paragraph
+//5. Set a text content to paragraph the fourth paragraph,Fourth Paragraph
+//6. Set id and class attribute for all the paragraphs using different attribute setting methods
