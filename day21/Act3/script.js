17610162abc2e31c45325246972aebb2b5b6fee0
PrimeFord <bashfordbash@gmail.com>
2022-12-19 12:48:35 +0100

    feat:Day21 Act3 Completed

---
+++ day21/Act3/script.js
@@ -0,0 +1,8 @@
+//DOM: Mini project 1
+//1. Develop the following application, use the following HTML elements to get started with. You will get the same code on starter folder. Apply all the styles and functionality using JavaScript only.
+
+//i.The year color is changing every 1 second
+//ii.The date and time background color is changing every on seconds
+//iii.Completed challenge has background green
+//iv.Ongoing challenge has background yellow
+//v.Coming challenges have background red
