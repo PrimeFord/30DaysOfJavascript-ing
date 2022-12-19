d3f877b8373289921b83b09c60433d4fa311af0f
PrimeFord <bashfordbash@gmail.com>
2022-12-19 14:28:57 +0100

    feat:Day3 Act1 Completed

--- day3/Act1/script.js
+++ day3/Act1/script.js
@@ -97,7 +97,7 @@ console.log(!"python".includes("on"));
 
 const today = new Date();
 console.log(today.getFullYear());
-console.log(today.getMonth());
+console.log(today.getMonth() + 1);
 console.log(today.getDate());
 console.log(today.getDay());
 console.log(today.getHours());
